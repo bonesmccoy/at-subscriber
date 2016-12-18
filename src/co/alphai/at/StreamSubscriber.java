41a5f40887804b637abbae8b177ca203987b5a67
Daniele Murroni <daniele.murroni@gmail.com>
2016-12-18 18:09:56 +0000

    First commit

---
+++ src/co/alphai/at/StreamSubscriber.java
@@ -0,0 +1,40 @@
+package co.alphai.at;
+
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class StreamSubscriber extends Thread
+{
+
+    public StreamSubscriber()
+    {
+        start();
+    }
+
+    public void run()
+    {
+        System.out.println("I'm running");
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        while(true)
+        {
+            try {
+                String line = br.readLine();
+                if (line.length() > 0) {
+                    if (line.startsWith("quit"))
+                        System.out.println("Bye");
+                        break;
+                }
+            } catch (IOException e) {
+                System.out.println("IO error trying to read your input!");
+            }
+        }
+    }
+
+    public static void main(String args[])
+    {
+        StreamSubscriber subscriber = new StreamSubscriber();
+    }
+}
