fe4c6e608e13ffa9635876ba20695c01fea09330
Daniele Murroni <daniele.murroni@gmail.com>
2016-12-21 17:54:47 +0000

    fixes on docker collector

--- collector/start.sh
+++ collector/start.sh
@@ -3,4 +3,4 @@
 echo "upgrading the database .."
 python3 manage.py db upgrade
 
-uwsgi --http :$UWSGI_PORT --wsgi-file app.py --callable connexion_application
+uwsgi --http :$UWSGI_PORT --wsgi-file app.py --callable connexion_application --stats :1717 --stats-http
