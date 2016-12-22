e340549dc9a011fbdec00ea9886782f685f2ded8
Daniele Murroni <daniele.murroni@gmail.com>
2016-12-22 16:08:39 +0000

    Start Unit Test

--- subscriber/src/main/java/org/bones/alphai/subscriber/stream/TradeUpdate.java
+++ subscriber/src/main/java/org/bones/alphai/subscriber/stream/TradeUpdate.java
@@ -1,15 +1,25 @@
 package org.bones.alphai.subscriber.stream;
 
+import at.feedapi.Helpers;
 import at.shared.ATServerAPIDefines;
-import at.utils.jlib.PrintfFormat;
+import at.shared.ActiveTick.DateTime;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.node.ObjectNode;
+
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.GregorianCalendar;
 
 
 public class TradeUpdate {
 
     private String symbol;
+    private short year;
+    private short month;
+    private short day;
     private short hour;
     private short minute;
     private short seconds;
@@ -17,6 +27,7 @@ public class TradeUpdate {
     private short pricePrecision;
     private double price;
     private long lastSize;
+    private Date timestamp;
 
     public TradeUpdate(ATServerAPIDefines.ATQUOTESTREAM_TRADE_UPDATE update)
     {
@@ -24,6 +35,9 @@ public class TradeUpdate {
         int plainSymbolIndex = strSymbol.indexOf((byte)0);
         symbol = strSymbol.substring(0, plainSymbolIndex);
 
+        year = update.lastDateTime.year;
+        month = update.lastDateTime.month;
+        day = update.lastDateTime.day;
         hour = update.lastDateTime.hour;
         minute = update.lastDateTime.minute;
         seconds = update.lastDateTime.second;
@@ -31,37 +45,43 @@ public class TradeUpdate {
         price = update.lastPrice.price;
         pricePrecision = update.lastPrice.precision;
         lastSize = update.lastSize;
+        timestamp = (new GregorianCalendar(year, (month - 1), day, hour, minute, seconds)).getTime();
     }
 
     public ObjectNode toJsonNode()
     {
         ObjectMapper mapper = new ObjectMapper();
-        ObjectNode payload = mapper.createObjectNode();
 
-        payload.put("symbol", symbol);
-        payload.put("hour", hour);
-        payload.put("minute", minute);
-        payload.put("seconds", seconds);
-        payload.put("milliseconds", milliseconds);
-        payload.put("price", price);
-        payload.put("lastSize", lastSize);
-
-        return payload;
+        return createObjectNode(mapper);
     }
 
     public String toJsonString() throws JsonProcessingException
     {
         ObjectMapper mapper = new ObjectMapper();
+        ObjectNode payload = createObjectNode(mapper);
+
+        return mapper.writeValueAsString(payload);
+    }
+
+    private ObjectNode createObjectNode(ObjectMapper mapper) {
+
         ObjectNode payload = mapper.createObjectNode();
 
         payload.put("symbol", symbol);
+        payload.put("year", year);
+        payload.put("month", month);
+        payload.put("day", day);
         payload.put("hour", hour);
         payload.put("minute", minute);
         payload.put("seconds", seconds);
         payload.put("milliseconds", milliseconds);
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+
+        payload.put("timestamp", formatter.format(timestamp));
         payload.put("price", price);
+        payload.put("precision", pricePrecision);
         payload.put("lastSize", lastSize);
 
-        return mapper.writeValueAsString(payload);
+        return payload;
     }
 }
---
+++ subscriber/src/test/java/org/bones/alphai/subscriber/stream/TestTradeUpdate.java
@@ -0,0 +1,52 @@
+package org.bones.alphai.subscriber.stream;
+
+import at.feedapi.Helpers;
+import at.shared.ATServerAPIDefines;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import org.junit.Assert;
+import org.junit.Test;
+
+
+public class TestTradeUpdate
+{
+    @Test
+    public void testToJsonNode()
+    {
+        ATServerAPIDefines defines = new ATServerAPIDefines();
+
+        ATServerAPIDefines.SYSTEMTIME timeOfEvent = defines.new SYSTEMTIME();
+        timeOfEvent.year = 2016;
+        timeOfEvent.month = 12;
+        timeOfEvent.day = 22;
+        timeOfEvent.hour = 12;
+        timeOfEvent.minute = 0;
+        timeOfEvent.second = 0;
+        timeOfEvent.milliseconds = 0;
+
+        ATServerAPIDefines.ATPRICE price = defines.new ATPRICE();
+        price.precision = 2;
+        price.price = 123.4567;
+
+
+        ATServerAPIDefines.ATQUOTESTREAM_TRADE_UPDATE updateFromServer = defines.new ATQUOTESTREAM_TRADE_UPDATE();
+        updateFromServer.lastDateTime = timeOfEvent;
+        updateFromServer.lastPrice = price;
+        updateFromServer.symbol = Helpers.StringToSymbol("AAPL");
+        updateFromServer.lastSize = 2000;
+
+        TradeUpdate trade = new TradeUpdate(updateFromServer);
+
+        Assert.assertTrue(trade.toJsonNode() instanceof ObjectNode);
+        try {
+            Assert.assertEquals(
+                    "{\"symbol\":\"AAPL\",\"year\":2016,\"month\":12,\"day\":22,\"hour\":12,\"minute\":0," +
+                            "\"seconds\":0,\"milliseconds\":0,\"timestamp\":\"2016-12-22 12:00:00\"," +
+                            "\"price\":123.4567,\"precision\":2,\"lastSize\":2000}",
+                    trade.toJsonString()
+            );
+        } catch (JsonProcessingException e) {
+            Assert.assertTrue(false);
+        }
+    }
+}
